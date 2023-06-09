3af0a93ae3d1373c7a2cded77d900eee71c50d2f
megajune <52547074+megajune@users.noreply.github.com>
2023-06-09 14:41:36 -0400

    Testing a linter

---
+++ testjava.js
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Before the script...</p>
+
+  <script>
+    const password = 'password';
+    alert( 'Hello, world!' );
+  </script>
+
+  <p>...After the script.</p>
+
+</body>
+
+</html>
