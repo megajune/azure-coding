09910e8e8d1cd6139902296279b49f33eca9e9e5
megajune <52547074+megajune@users.noreply.github.com>
2023-06-13 10:47:53 -0400

    gogogo

--- Powershell/vnet.ps1
+++ Powershell/vnet.ps1
@@ -1,16 +1,29 @@
+#./vnet.ps1 -vnetName "test1212" -AddressPrefix "10.212.0.0/24"
 param(
-[string]$vnetName
+[Parameter(Mandatory, HelpMessage = 'Please enter a subnet in the form a.b.c.d/#', ValueFromPipeline, Position = 0)]
+[string]$vnetName,
+[string]$AddressPrefix
 )
 $rg = Get-AzureRMResourceGroup
+
+if ($AddressPrefix -match '^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}$') {
+    #Split IP and subnet
+    $IP = ($AddressPrefix -split '\/')[0]
+    [int] $SubnetBits = ($AddressPrefix -split '\/')[1]
+    if ($SubnetBits -lt 7 -or $SubnetBits -gt 30) {
+        Write-Error -Message 'The network mask (in CIDR notation) following the / must be between 7 and 30'
+        break
+    }
+    $Octets = $IP -split '\.'
+    $networkAddressNumber = $Octets[0]+'.'+$Octets[1]+'.'+$Octets[2]
+} else {
+    Write-Error -Message "Subnet [$AddressPrefix] is not in a valid format"
+    break
+}
 
 <#param (
     [string]$vnetName = "vnet-1",
     [Parameter(Mandatory=$true)][string]$vnetName
-    #[string]$password = $( Read-Host "Input password, please" )
-)
-param (
-    [string]$addressPrefix = "10.212.0.0/24",
-    [Parameter(Mandatory=$true)][string]$addressPrefix
     #[string]$password = $( Read-Host "Input password, please" )
 )#>
 
@@ -21,3 +34,15 @@ $vnet = @{
     AddressPrefix = $addressPrefix
 }
 $virtualNetwork = New-AzVirtualNetwork @vnet
+
+$subnetnames = 'AzureFirewallSubnet','AzureBastionSubnet','WorkloadSubnet'
+
+
+foreach ($name in $subnetnames){
+$subnet = @{
+    Name = $name
+    VirtualNetwork = $virtualNetwork
+    AddressPrefix = $networkAddressNumber+'.0/28'
+}
+$subnetConfig = Add-AzVirtualNetworkSubnetConfig @subnet
+}
