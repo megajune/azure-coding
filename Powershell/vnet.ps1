23a521c05d2844a46d064583aa3e61570eca9b45
megajune <52547074+megajune@users.noreply.github.com>
2023-06-13 10:03:08 -0400

    DO it

--- Powershell/vnet.ps1
+++ Powershell/vnet.ps1
@@ -1,10 +1,20 @@
 $rg = Get-AzureRMResourceGroup
 $rg.name
 
+param (
+    [string]$vnetName = "vnet-1",
+    [Parameter(Mandatory=$true)][string]$vnetName
+    #[string]$password = $( Read-Host "Input password, please" )
+)
+param (
+    [string]$addressPrefix = "10.212.0.0/24",
+    [Parameter(Mandatory=$true)][string]$addressPrefix
+    #[string]$password = $( Read-Host "Input password, please" )
+)
 $vnet = @{
-    Name = 'vnet-1'
+    Name = $vnetName
     ResourceGroupName = $rg.ResourceGroupName
     Location = $rg.Location
-    AddressPrefix = '10.0.0.0/16'
+    AddressPrefix = $addressPrefix
 }
 $virtualNetwork = New-AzVirtualNetwork @vnet
