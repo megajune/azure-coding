ce709b7292f67b66d758277a719dd90181ad54fb
Jonathan Martin <megajune@gmail.com>
2023-07-14 10:15:36 -0400

    Adding notes

--- DevEnv/installscript.sh
+++ DevEnv/installscript.sh
@@ -4,7 +4,7 @@ cd /tmp
 
 sudo apt update
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -f
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -12,7 +12,10 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 
 apt-cache policy docker-ce
 
-sudo apt install docker-ce
+sudo apt install docker-ce -f
 
 sudo systemctl status docker
 
+#apt-get install resolvconf
+
+#location of Nessus in Docker = docker.io/tenable/nessus:latest-ubuntu
